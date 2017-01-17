4348cee36b754e6686edd929177501b0979c124d
Seung-Zin Nam <zin@mcw.de>
2017-01-17 22:13:55 +0100

    replace multiline anchors which are deprecated in RoR 4

--- app/models/anonymous_watcher.rb
+++ app/models/anonymous_watcher.rb
@@ -5,7 +5,7 @@ class AnonymousWatcher < ActiveRecord::Base
 
   validates_presence_of :mail
   validates_uniqueness_of :mail, :scope => [:watchable_type, :watchable_id], :allow_blank => true
-  validates_format_of :mail, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :allow_blank => true
+  validates_format_of :mail, :with => /\A([\A@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :allow_blank => true
 
   def anonymous?
     true
